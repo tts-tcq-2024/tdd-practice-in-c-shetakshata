ea653e73ff898b57cc93268b82672805ad0c0ef3
shetakshata <akshatashet08@gmail.com>
2024-07-16 14:57:43 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,22 +9,3 @@ int check_empty(const char* ch) {
     }
     return 0;  // Not empty
 }
-// Function: add_arr
-// Adds up integers in the array 'arr' with 'cnt' elements, ignoring values >= 1000.
-// Returns the sum of valid integers.
-int add_arr(int *arr,int cnt)
-{
-    int i = 0;
-    int total = 0;
-    while(i < cnt)
-    {
-        if(arr[i] < 1000)
-        {
-          total += arr[i];
-        }
-        i++;
-    }
-
-    return total;
-}
-
