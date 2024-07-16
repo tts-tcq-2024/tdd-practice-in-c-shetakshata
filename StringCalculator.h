06682ae4fa272c5caeba750b35395488100bae53
shetakshata <akshatashet08@gmail.com>
2024-07-16 15:05:01 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,11 +1,6 @@
-// Function: check_empty
-// Checks if 'ch' is an empty string.
-// Returns 1 if 'ch' is empty, otherwise returns 0.
-int check_empty(const char* ch) {
-    int strlength = strlen(ch);
-
-    if (strlength == 0) {
-        return 1;  // Empty string
+int add(const char* input){
+    if((input== NULL || input[0]=='\0')){
+return 0
     }
-    return 0;  // Not empty
+return -1;
 }
