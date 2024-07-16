06c42f39a9d057d8cf60e6a55f0140e4276d986e
shetakshata <akshatashet08@gmail.com>
2024-07-16 15:18:29 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,3 +4,11 @@ return 0;
     }
 return -1;
 }
+// Function to convert string to integer and calculate sum if less than 1000
+int LessThanThousand(const char *num) {
+    int input = atoi(num); // Convert string to integer
+    if (input < 1000) {
+        return input;
+    }
+    return 0;
+}
