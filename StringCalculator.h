2079f9d8134fe925283b07ef6fd8562ea281ed21
shetakshata <akshatashet08@gmail.com>
2024-07-16 14:41:38 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,16 @@
+int add_arr(int *arr,int cnt)
+{
+    int i = 0;
+    int total = 0;
+    while(i < cnt)
+    {
+        if(arr[i] < 1000)
+        {
+          total += arr[i];
+        }
+        i++;
+    }
+
+    return total;
+}
 
